b26b2497f7ce9508e9d85fe778cb235d592f9ca7
Raidren <90018704+Raidren@users.noreply.github.com>
2021-09-18 00:59:28 +0500

    commit

---
+++ 2-Zadanie.py
@@ -0,0 +1,7 @@
+from collections import Counter
+f = open('test.txt','r')
+f1 = f.read()
+print(f1)
+cnt = Counter(f1.replace(' ', ''))
+
+print('Часто встречаемый символ в файле это: ',cnt.most_common()[0][0])
